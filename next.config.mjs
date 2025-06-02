0ad7803ac17983ced9fb84b1609fd88d00580a61
sowrabb <svokkalkar@gmail.com>
2025-06-02 12:55:29 +0530

    fix: Refine asset prefixing for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,13 @@
 const nextConfig = {
   output: 'export', // Enables static HTML export
   basePath: process.env.NODE_ENV === 'production' ? '/cybergaurd' : '', // Adjust for repository name
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/cybergaurd/' : '', // Adjust for repository name
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/cybergaurd/' : '', // Let basePath handle asset prefixing
   images: {
     unoptimized: true, // Necessary for static export with next/image
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
     ],
