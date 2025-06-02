beb2cb4d7bc46a7cfbc50e7ced5f0f2a8dc9a483
sowrabb <svokkalkar@gmail.com>
2025-06-02 13:01:21 +0530

    fix: Use assetPrefix only for GitHub Pages paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static HTML export
-  basePath: process.env.NODE_ENV === 'production' ? '/cybergaurd' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/cybergaurd/' : undefined, // Explicitly set for production
+  // basePath: process.env.NODE_ENV === 'production' ? '/cybergaurd' : '', // Try with assetPrefix only for production
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/cybergaurd/' : undefined,
   images: {
     unoptimized: true, // Necessary for static export with next/image
     remotePatterns: [
